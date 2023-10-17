9095512b61eeddefdb846e04c99c75024f4af20f
Eric Frey <ericlfrey@gmail.com>
2023-10-16 21:55:12 -0500

    Progress.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const pets = [
     color: "Brown",
     specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
     type: "dino",
-    imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg",
+    imageUrl: "https://loremflickr.com/320/240/t-rex",
   },
   {
     id: 3,
@@ -61,7 +61,7 @@ const pets = [
     color: "Grey",
     specialSkill: "Comfortable in the outdoors for up to eight hours.",
     type: "dino",
-    imageUrl: "http://www.theouthousers.com/images/jck//ThanosCopter/news/grumpasaur.jpg"
+    imageUrl: "https://loremflickr.com/320/240/dinosaur"
   },
   {
     id: 9,
@@ -85,7 +85,7 @@ const pets = [
     color: "Orange",
     specialSkill: "Can be around food without staring creepily at it.",
     type: "dino",
-    imageUrl: "http://www.cantref.com/userfiles/events/walking%20dino.jpg?w=600"
+    imageUrl: "https://loremflickr.com/320/240/dino"
   },
   {
     id: 12,
@@ -101,7 +101,7 @@ const pets = [
     color: "Red",
     specialSkill: "Expertly quotes and recognizes dialogue from early seasons of The Simpsons.",
     type: "dog",
-    imageUrl: "http://1kjkdg1axrkd2g03cnboj761.wpengine.netdna-cdn.com/wp-content/uploads/2017/12/braydon-anderson-105552-e1512684107659.jpg"
+    imageUrl: "https://loremflickr.com/320/240/dog"
   },
   {
     id: 14,
@@ -125,7 +125,7 @@ const pets = [
     color: "Brown",
     specialSkill: "Drives at a safe rate of speed in snow or rain.",
     type: "dino",
-    imageUrl: "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg"
+    imageUrl: "https://loremflickr.com/320/240/stegosaurus"
   },
   {
     id: 17,
@@ -133,7 +133,7 @@ const pets = [
     color: "Yellow",
     specialSkill: "Does not freak out if you haven’t seen his favorite movie (The Big Lebowski).",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
+    imageUrl: "https://loremflickr.com/320/240/kitty"
   },
   {
     id: 18,
@@ -141,7 +141,7 @@ const pets = [
     color: "Brown",
     specialSkill: "Proficient in air guitar",
     type: "dino",
-    imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
+    imageUrl: "https://loremflickr.com/320/240/brontosaurus"
   },
   {
     id: 19,
@@ -149,7 +149,7 @@ const pets = [
     color: "Blue",
     specialSkill: "Listens attentively to boring stories.",
     type: "dog",
-    imageUrl: "http://dailynewsdig.com/wp-content/uploads/2014/03/Creative-And-Funny-Dog-Stock-Photography-Pictures-2.jpg"
+    imageUrl: "https://loremflickr.com/320/240/puppy"
   },
   {
     id: 20,
@@ -157,7 +157,7 @@ const pets = [
     color: "Black",
     specialSkill: "Uses litter box at appropriate hours.",
     type: "cat",
-    imageUrl: "http://www.funnycatsite.com/pictures/Lazy_White_Cat.jpg"
+    imageUrl: "https://loremflickr.com/320/240/cat"
   },
   {
     id: 21,
@@ -165,7 +165,7 @@ const pets = [
     color: "Red",
     specialSkill: "Owns a Nintendo Power Glove.",
     type: "dino",
-    imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
+    imageUrl: "https://loremflickr.com/320/240/jurassic"
   },
   {
     id: 22,
@@ -241,4 +241,31 @@ const pets = [
   }
 ];
 
-console.log('pets', pets);
+document.querySelector('#app').innerHTML = `
+<h1>Pet Adoption</h1>
+<div id="petContainer" class="pet-container"></div>
+`;
+
+const htmlOnDOM = (divId, str) => {
+  document.getElementById(divId).innerHTML = str;
+};
+
+const petsOnDom = () => {
+  let domString = '';
+  pets.forEach((pet) => {
+    domString += `
+    <div class="card" style="width: 15rem;">
+      <div class="card-header">${pet.name}</div>
+      <img src="${pet.imageUrl}"  alt="image of a ${pet.type}">
+      <div class="card-body">
+        <p class="card-title">Color: ${pet.color}</p>
+        <p class="card-text">${pet.specialSkill}</p>
+      </div>
+      <div class="card-footer ${pet.type}-color">${pet.type.charAt(0).toUpperCase() + pet.type.slice(1)}</div>
+    </div>
+    `;
+  });
+  htmlOnDOM('petContainer', domString);
+};
+
+petsOnDom();
